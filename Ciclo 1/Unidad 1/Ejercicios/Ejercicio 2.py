145c15ec6c6be2a9988c7fd94aa2f54401242ead
carlos villalba <caven908mail.com>
2022-05-19 20:09:32 -0500

    desarrollo nuevos ejercicios

---
+++ Ciclo 1/Unidad 1/Ejercicios/Ejercicio 2.py
@@ -0,0 +1,16 @@
+# Escribir un programa que almacene la cadena de caracteres contraseña en una variable,
+# pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida 
+# por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+
+contraseña=input("Ingrese la contraseña: ")
+contraseña2=input("Recti la de nuevo: ")
+
+if contraseña == contraseña2:
+    print("Las contraseñas coinciden")
+    
+else:
+    print("Las contraseñas no coinciden")
+
+
+
+
